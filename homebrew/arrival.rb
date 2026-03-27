08883df2eb4f4a22509d9fd99ab0cd4acab4fdb7
Kevin Guebert <kevin@wattch.io>
2026-03-26 21:44:16 -0400

    chore: update appcast and cask for v1.1.0 [skip ci]

--- homebrew/arrival.rb
+++ homebrew/arrival.rb
@@ -1,6 +1,6 @@
 cask "arrival" do
   version "1.1.0"
-  sha256 "c67b3cb2dec7d9ecb90b9ff5c6c746e137beccf3656979e38791a333b452e00f"
+  sha256 "690b6fbc20bd0227f2181137fb575c12d2c01a4aa35d2ab722054070e44fe8c6"
 
   url "https://github.com/kevinguebert/arrival/releases/download/v#{version}/Arrival-#{version}.dmg"
   name "Arrival"
